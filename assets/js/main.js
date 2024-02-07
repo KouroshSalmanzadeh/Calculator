4e5c577c4c9d5c1f4dc71a93b341d6675a5b4e23
KouroshSalmanzadeh <kouroshsalmanzadeh@gmail.com>
2024-02-08 00:42:22 +0330

    Debug and optimize performance #7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,31 @@
-var firstNumber, secondNumber, globalType, flag = false, total = false;
+var firstNumber = 0,
+    secondNumber = 0,
+    globalType, flag, total, back = false;
 
 function getNumber(ev) {
     if (firstNumber && flag == true) {
         document.querySelector("input").value = '';
         flag = false;
+        back = false;
+    } else if (total == true) {
+        document.querySelector("input").value = 0;
+        firstNumber = 0;
+        secondNumber = 0;
+        total = false;
     }
     document.querySelector("input").value += ev.currentTarget.innerText;
 };
 
 function oprate(type) {
 
-    if(type != "result"){
+    if (type != "result") {
         firstNumber = +document.querySelector("input").value;
         globalType = type;
         flag = true;
-    }else {
+        back = true;
+    } else {
         secondNumber = +document.querySelector("input").value;
-        switch (globalType){
+        switch (globalType) {
             case "divid":
                 document.querySelector("input").value = firstNumber / secondNumber;
                 total = true;
@@ -42,24 +51,29 @@ function oprate(type) {
 
 function Clear(cleartype) {
     if (cleartype == 'clear all') {
-        document.querySelector("input").value = '';
+        document.querySelector("input").value = 0;
         firstNumber = 0;
         secondNumber = 0;
-    }else if (cleartype == 'clear this number'){
-        if (firstNumber != ''){
-            secondNumber = '';
+        total = false;
+    } else if (cleartype == 'clear this number') {
+        if (firstNumber != 0) {
+            secondNumber = 0;
             document.querySelector("input").value = '';
-        }else {
-            firstNumber = '';
+        } else {
+            firstNumber = 0;
             document.querySelector("input").value = '';
         }
-    }else {
-        if(total != true){
-            document.querySelector("input").value = parseInt(document.querySelector("input").value / 10);
-            if (document.querySelector("input").value == 0){
-            document.querySelector("input").value = '';
+    } else {
+        if (total != true) {
+            if (firstNumber !== 0 && back == true) {
+                window.alert("You can't clear your first number! please enter .C or CE");
+            } else if (back == false && secondNumber == 0) {
+                document.querySelector("input").value = parseInt(document.querySelector("input").value / 10);
+                if (document.querySelector("input").value == 0) {
+                    document.querySelector("input").value = '';
+                }
             }
-        }else {
+        } else {
             window.alert("You can't back! please enter .C or CE");
         }
     }
